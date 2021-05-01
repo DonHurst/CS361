b7fa1da7e715c6b482d10e2956d1e8c98d028ab8
DonHurst <59455598+DonHurst@users.noreply.github.com>
2021-04-30 19:30:45 -0700

    added html page, styling, and some js stuff like the dropzone. currently working to accept only .json files as expected

---
+++ app.js
@@ -0,0 +1,24 @@
+Dropzone.options.myDropzone= {
+    autoProcessQueue: false,
+    uploadMultiple: false,
+    acceptedFiles: '.json',
+    maxFiles: 1,
+    addRemoveLinks: true,
+    init: function() {
+        dzClosure = this; // Makes sure that 'this' is understood inside the functions below.
+
+        // for Dropzone to process the queue (instead of default form behavior):
+        document.getElementById("submit-all").addEventListener("click", function(e) {
+            // Make sure that the form isn't actually being sent.
+            e.preventDefault();
+            e.stopPropagation();
+            dzClosure.processQueue();
+        });
+
+        //send all the form data along with the files:
+        this.on("sendingmultiple", function(data, xhr, formData) {
+            formData.append("firstname", jQuery("#firstname").val());
+            formData.append("lastname", jQuery("#lastname").val());
+        });
+    }
+}
