d5933992af9d8f1c8f8aa2966a565ba9c5ef4dd1
DonHurst <59455598+DonHurst@users.noreply.github.com>
2021-05-07 17:07:55 -0700

    Fixed dropzone, files now saving successfully on the server

--- python stuff/app.py
+++ python stuff/app.py
@@ -7,27 +7,29 @@ from flask_dropzone import Dropzone
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+# Configuring drop zone. Upload path is a dir called uploads, max file size 1gb, max timeout 3 mins, only allowing json files
 app.config.update(
-
-    # Configuring drop zone. Upload path is a dir called uploads, max file size 1gb, max timeout 3 mins, only allowing json files
-
-    UPLOAD_PATH = os.path.join(basedir,'uploads'),
+    UPLOADED_PATH = os.path.join(basedir,'uploads'),
     DROPZONE_MAX_FILE_SIZE = 1024,
     DROPZONE_TIMEOUT = 3*60*1000,
     DROPZONE_ALLOWED_FILE_CUSTOM = True,
     DROPZONE_ALLOWED_FILE_TYPE = '.json',
-    DROPZONE_MAX_FILES = 1 
+    DROPZONE_MAX_FILES = 1
 )
 
 dropzone = Dropzone(app)
 
-@app.route("/", methods=["GET", "POST"])
-@app.route("/home", methods=["GET", "POST"])
-def home():
+@app.route("/", methods=['POST', 'GET'])
+def upload():
 
-    # The flask dropzone POST code was borrowed from the documentation https://flask-dropzone.readthedocs.io/en/latest/basic.html
+    # The flask dropzone code was borrowed from the documentation https://flask-dropzone.readthedocs.io/en/latest/basic.html
     if request.method == 'POST':
-        f = request.files.get('file')
-        f.save(os.path.join(app.config['UPLOAD_PATH'], f.filename))
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
 
-    return render_template('home.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+
